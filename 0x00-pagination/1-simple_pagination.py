74578e42fefdde4eac7429f0b59ef5c53646c2eb
geno <yvesgeno@outlook.com>
2024-01-29 19:32:25 +0200

    task 1 correction

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -26,7 +26,7 @@ class Server:
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """ Get page from csv data """
-        assert type(page) == int and type(page_size) == int
+        assert int == type(page) and type(page_size) == int
         assert page > 0 and page_size > 0
         start, end = index_range(page, page_size)
         return self.dataset()[start:end]
